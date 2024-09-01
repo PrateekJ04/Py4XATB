72515ab36ff25d8b54d68e627f5bc3bb5c802d14
PrateekJ04 <jprat786@gmail.com>
2024-09-01 20:47:56 +0530

    Second Commit with sum function

---
+++ PyPrac_06_Aug/PracSum.py
@@ -0,0 +1,8 @@
+def sum_function():
+    a= int(input("Enter a integer value for a: "))
+    b= int(input("Enter a integer value for b: "))
+    result= a+b
+    print("The sum of number is: ", result)
+    return result
+
+sum_function()
