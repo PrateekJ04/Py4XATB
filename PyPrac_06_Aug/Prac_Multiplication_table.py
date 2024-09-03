8cc990885e0950d8beaab06ed7f612d8193cf8ea
PrateekJ04 <jprat786@gmail.com>
2024-09-03 17:40:54 +0530

    Added user input

--- PyPrac_06_Aug/Prac_Multiplication_table.py
+++ PyPrac_06_Aug/Prac_Multiplication_table.py
@@ -1,8 +1,9 @@
 # Home Can you create a Program I will give you number(userinput and print table)
 #Create a table of integer
-def table_of_a_number(t):
+def table_of_a_number():
+    t = int(input("ENTER A VALID INTEGER: "))
     print(f"Multiplication Table for {t} is : ")
     for i in range (1,11):
         print(f"{t} x {i} = {t * i}")
 
-table_of_a_number(5)
+table_of_a_number()
