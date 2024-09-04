e2182e8cbcb83ef411ec565d374b0a8f53203d53
PrateekJ04 <jprat786@gmail.com>
2024-09-04 22:43:00 +0530

    Square and Cube of a Number

---
+++ PyPrac_17_Aug/Prac_Sq_and_Cube_Num.py
@@ -0,0 +1,7 @@
+# Develop a Python script that calculates the square and cube of a given number.
+
+num= int(input("Enter a number: "))
+square_of_num= pow(num,2)
+cube_of_num= pow(num,3)
+
+print("The Square of a given number is: "f"{square_of_num}\n"f"The Cube of a given number is: {cube_of_num}")
